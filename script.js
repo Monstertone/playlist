b2b18e55e739a3941567ca40eefe36a036ff17af
Thomas Gabauer <tgabauer@gmail.com>
2018-08-24 22:48:58 -0700

    added javascript to generate 3 random album covers

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ window.onload = function(e) {
 
   axios.get('https://lit-fortress-6467.herokuapp.com/object').then(function(response){
 
-    console.log(response.data);
+    console.log(response.data.results[0]['cover_art']);
+
+  // GENERATING 3 UNIQUE NUMBERS FOR ALBUM DISPLAY
+
+    let randomId = [];
+   //  let arrLength = randomId.length;
+  while (randomId.length < 3){
+    let x = Math.floor(Math.random() * 5)
+    if (!randomId.length) {
+      randomId.push(x);
+    } else if (x !== randomId[(randomId.length - 1)] && x !== randomId[(randomId.length - 2)]) {
+    randomId.push(x);
+    }
+  }
+
+  console.log(randomId);
+
+
   });
 
 };
