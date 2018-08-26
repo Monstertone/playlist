06d64e68420d97d90c800635281dfe1cb6565371
Thomas Gabauer <tgabauer@gmail.com>
2018-08-26 00:25:07 -0700

    all functionality now working. Will clean up further tomorrow

--- playlist.js
+++ playlist.js
@@ -2,7 +2,7 @@ window.onload = function(e) {
   var playDiv = document.querySelector('.nowPlaying')
   axios.get('https://lit-fortress-6467.herokuapp.com/object').then(function(response){
 
-    console.log(response.data);
+    console.log(response);
 
 
     // console.log(response.data.results[0]['cover_art']);
@@ -20,7 +20,7 @@ window.onload = function(e) {
            albGallery.appendChild(albumMini);
     }
 
-
+    var binArray = [];
 
     albGallery.addEventListener('click',  function(event){
 
@@ -31,26 +31,45 @@ window.onload = function(e) {
           if (output['id'] == event.target['id']){
           // console.log(output['artist'] + ': '+ output['title']);
           let playText = {};
+
           // playText = output['artist'] + ': '+ output['title']
           // playText = output['artist'] output['title']
            playText.artist = output['artist']
            playText.title = output['title']
           // playDiv.innerHTML = playDiv.innerHTML  + `<p> ${playText}`;
           playDiv.innerHTML = `${playDiv.innerHTML  + playText.artist +': '+ playText.title}  <BR>`;
-
+          // playDiv.innerHTML = playDiv.innerHTML  + playText.artist +': '+ playText.title  <BR>
+           binArray.push(playText.title);
           }
 
        });
 
     })
+          console.log(binArray);
 
+              let submitAlbums = document.getElementById('submitBtn')
+                submitAlbums.addEventListener('click', function(){
+                axios.post(`https://lit-fortress-6467.herokuapp.com/post`, {
+                binArray
+                }).then(function(response){
+
+                  console.log(response);
+                });
+              });
 
    });
 
-   let clearList = document.getElementById('clearBtn');
-   clearList.addEventListener('click', function(){
-   playDiv.innerHTML = "";
- });
+
+
+
+
+         let clearList = document.getElementById('clearBtn');
+
+         clearList.addEventListener('click', function(){
+
+             playDiv.innerHTML = "";
+
+          });
 
 
 }
